5b9d2c1062497919a2a67ced219718bed451b999
Gustavo Sousa <gustavohl98@gmail.com>
2021-05-06 16:02:02 -0300

    Exercicio 2 - Adiciona 3 links dentro de BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
         <Home />
       </BrowserRouter>
     );
