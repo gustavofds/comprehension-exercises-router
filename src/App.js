347102377bbe0cab7335a92429bcdfcfc12e3772
Gustavo Sousa <gustavohl98@gmail.com>
2021-05-06 16:10:37 -0300

    Exercicio 4 - mapeia rota para /about

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ class App extends Component {
         <Link to="/about">About</Link>
         <Link to="/users">Users</Link>
         <Route path="/" component={ Home }></Route>
+        <Route path="/about" component={ About }></Route>
       </BrowserRouter>
     );
   }
