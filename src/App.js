dd48c6fcbff1f1e89b74857dd3e0dfe4eb818d4d
Gustavo Sousa <gustavohl98@gmail.com>
2021-05-06 16:12:14 -0300

    Exercicio 6 - Adiciona rota para /users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
+import Users from './components/Users';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ class App extends Component {
         <Link to="/users">Users</Link>
         <Route exact path="/" component={ Home }></Route>
         <Route path="/about" component={ About }></Route>
+        <Route path="/users" component={ Users }></Route>
       </BrowserRouter>
     );
   }
